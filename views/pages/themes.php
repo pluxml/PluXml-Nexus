75a8e14e49d426e83baebd80273feec13a407dcb
P3ter <contact@p3ter.fr>
2020-04-30 14:39:12 +0200

    view themes list

--- views/pages/themes.php
+++ views/pages/themes.php
@@ -1,5 +1,5 @@
 <h1>Themes</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et leo et ex congue blandit. Sed vel eleifend lorem. Proin laoreet commodo libero non dictum. Vestibulum at bibendum nunc, a ullamcorper turpis. Vestibulum sed libero lacus. Nulla urna lectus, viverra id arcu ac, tempus aliquet elit. Donec aliquam et nisl eu rhoncus. Maecenas semper urna mauris, a aliquam orci sagittis vel. Cras neque neque, porta a sagittis ac, sodales id nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc in erat augue. Vestibulum a ipsum eu massa finibus fringilla.</p>
 
-<p><a href="<?= $routerService->urlFor('theme', ['name' => $themes[0]['name']]) ?>"><?= $themes[0]['name'] ?></a></p>
-<p><a href="<?= $routerService->urlFor('theme', ['name' => $themes[1]['name']]) ?>"><?= $themes[1]['name'] ?></a></p>
+<? foreach ($themes as $key => $theme): ?>
+    <p><a href="<?= $routerService->urlFor('theme', ['name' => $theme['name']]) ?>"><?= $theme['name'] ?></a></p>
+<?php endforeach; ?>
