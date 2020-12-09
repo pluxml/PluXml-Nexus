10932fe485efbabd6b276bb2dde07e2fc2ee32dc
Pedro CADETE <pcadete@conforama.fr>
2020-12-09 19:16:57 +0100

    fix profiles view

--- views/pages/profiles.php
+++ views/pages/profiles.php
@@ -3,11 +3,12 @@
 
     <div class="page">
         <h2>Profiles</h2>
-        <p>
-            <a href="<?= $routerService->urlFor('profile', ['username' => $profiles[0]['username']]) ?>"><?= $profiles[0]['username'] ?></a>
-        </p>
-        <p>
-            <a href="<?= $routerService->urlFor('profile', ['username' => $profiles[1]['username']]) ?>"><?= $profiles[1]['username'] ?></a>
-        </p>
+        <ul>
+            <?php foreach ($profiles as $profile): ?>
+                <li>
+                    <a href="<?= $routerService->urlFor('profile', ['username' => $profile['username']]) ?>"><?= $profile['username'] ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </div>
